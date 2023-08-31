2188bb6c3e9d6c891ed8557e86e5b3b2fa0b9358
aronno1912 <debjanyaronno@gmail.com>
2023-09-01 01:18:38 +0600

    nurse table ,checkupitem table added

--- belasheshe/nurse/models.py
+++ belasheshe/nurse/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from viewflow.fields import CompositeKey
 # Create your models here.
+class Nurse(models.Model):
+    Nurse_Id = models.AutoField(primary_key=True)
+    Qualifications = models.CharField(max_length=50)
+    Shift = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Checkup {self.Nurse_Id}"
+
+    #python manage.py makemigrations
+#    python manage.py migrate
--- belasheshe/residents/models.py
+++ belasheshe/residents/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from viewflow.fields import CompositeKey
 # from healthcare.models import ResidentRoomAsgn, Doctor, Nurse, MedCond
 # from .member import Member
-
+from nurse.models import Nurse
+#from doctors.models import Doctor
 class Member(models.Model):
     Member_ID = models.IntegerField(primary_key=True, max_length=10)
     # Room_no = models.ForeignKey(ResidentRoomAsgn, on_delete=models.CASCADE)
@@ -42,3 +43,25 @@ class MemberAppoint(models.Model):
 
     def __str__(self):
         return f"Appointment {self.App_ID} for {self.Member_ID}"
+
+class CheckupItem(models.Model):
+    Checkup_Id = models.AutoField(primary_key=True)
+    Blood_Pressure = models.CharField(max_length=20)
+    Sugar = models.DecimalField(max_digits=5, decimal_places=2)
+    Heartrate = models.IntegerField()
+
+    def __str__(self):
+        return f"Checkup {self.Checkup_Id}"
+
+
+# class SpecialCheckupSchedule(models.Model):
+#     Special_Checkup_Id = models.OneToOneField(CheckupItem, primary_key=True, on_delete=models.CASCADE)
+#     Member_Id = models.ForeignKey(Member, on_delete=models.CASCADE)
+#     Nurse_Id = models.ForeignKey(Nurse, on_delete=models.CASCADE)
+#     Doctor_Id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+#     Date = models.DateField()
+#     Frequency = models.PositiveIntegerField()
+#     Completed = models.BooleanField(default=False)
+#
+#     def __str__(self):
+#         return f"Special Checkup Schedule for {self.Special_Checkup_Id}"
